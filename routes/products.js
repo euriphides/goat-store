0e83df59693e8d58289d53e4856a72687891c0b2
euriphides <euriphides@gmail.com>
2018-03-04 17:35:56 -0700

    Add error handling to routes/product

--- routes/products.js
+++ routes/products.js
@@ -2,13 +2,23 @@ const Product = require('../models/product')
 
 module.exports = (app) => {
   app.get('/api/v1/products', async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
+    try {
+      const products = await Product.find({})
+      res.json(products)
+    } catch (e) {
+      res.status(500)
+      res.json({error: 'No Products Found'})
+    }
   })
   app.get('/api/v1/products/:id', async (req, res) => {
-    const product = await Product.findOne({
-      _id: req.params.id
-    })
-    res.json(product)
+    try {
+      const product = await Product.findOne({
+        _id: req.params.id
+      })
+      res.json(product)
+    } catch (e) {
+      res.status(404)
+      res.json({error: 'Not Found'})
+    }
   })
 }
